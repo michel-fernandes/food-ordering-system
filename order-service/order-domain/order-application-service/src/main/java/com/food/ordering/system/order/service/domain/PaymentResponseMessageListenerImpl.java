501d6b63b0f831c674c826d24bfa9371c93f35cd
MICHEL A FERNANDES <61243956+michel-fernandes@users.noreply.github.com>
2023-05-28 15:55:56 -0300

    completed implement service from domain layer. It is necessary continue to implemmentaion the SAGA pattern

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Slf4j
+@Service
+//availible this package only, therefor it can be used by the interfaces
+class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+
+    }
+
+    @Override
+    public void paymentCncelled(PaymentResponse paymentResponse) {
+
+    }
+}
