3248636015d7ff0a6d580652978d1530d2b69499
MICHEL A FERNANDES <61243956+michel-fernandes@users.noreply.github.com>
2023-06-10 18:44:30 -0300

    implemented CustomerServiceApplication and create table, materialized view and insert customers

---
+++ customer-services/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES ('f1a86dae-07d4-11ee-be56-0242ac120002', 'michel.fernandes', 'Michel', 'Fernandes');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('f1a87114-07d4-11ee-be56-0242ac120002', 'lucas.kuca.beludo', 'Lucas Kuca', 'Beludo');
